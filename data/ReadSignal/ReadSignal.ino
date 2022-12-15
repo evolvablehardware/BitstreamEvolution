09b93bf58768877a7e21c43c47433934cefc936f
rhit-manthelt <98466761+rhit-manthelt@users.noreply.github.com>
2022-12-15 10:36:30 -0500

    Merged and Fixed Arduino Code

--- data/ReadSignal/ReadSignal.ino
+++ data/ReadSignal/ReadSignal.ino
@@ -1,10 +1,13 @@
 /*
-
 Read analog voltage value from analog IN (pin 2)
 Convert it to digital value and transmit over serial
 
 The delays are used to control for noise on the serial line.
 */
+
+#define pulseWidthMeasureSelection '3'
+#define pulseCountMeasureSelection '1'
+#define ADCMeasureSelection '2' 
 
 
 const int analogPin = A0;
@@ -17,34 +20,43 @@ unsigned long pulseLength = 0;
 int sampleNumber = 5;
 int x = 0;
 
+/* ---------------------------------- Setup --------------------------------- */
 void setup(){
+    
+    /*  Set analog reference point, 
+        external uses the voltage applied to the AREF pin
+    */
     //analogReference(EXTERNAL);
+    
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(interrupt, OUTPUT);
+
     digitalWrite(LED_BUILTIN, LOW);
     Serial.begin(115200);
     Serial.println("Began serial");
     while (!Serial);
     digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(2, HIGH); // noise suppression on interrupt pin
-    attachInterrupt(digitalPinToInterrupt(interrupt),pulseCounter, RISING); // can be changed from RISING to FALLING or CHANGE
+
+    // noise suppression on interrupt pin
+    digitalWrite(2, HIGH); 
+
+    // can be changed from RISING to FALLING or CHANGE
+    attachInterrupt(digitalPinToInterrupt(interrupt),pulseCounter, RISING); 
 }
 
+
+// This method is attached to the interrupt to count pulses
 void pulseCounter(){
     pulseCount++;
 }
 
+/* ---------------------------------- Loop ---------------------------------- */
 void loop(){
     if ((x = Serial.available()) > 0){
       int x = Serial.read();
       
-      if (x == '2'){
+      if (x == ADCMeasureSelection){
         Serial.print("START\nSTART\nSTART\n");
-
-//        for(int t=0; t<=499; t++){
-//          buf[t] = analogRead(analogPin); //Read the AnalogPin
-//          delayMicroseconds(10); //Wait 10 uS
-//        }
 
         for(int i=0; i<=499; i++){
             Serial.print(i+1);
@@ -59,30 +71,41 @@ void loop(){
         Serial.print("FINISHED\nFINISHED\nFINISHED\n");
         delay(10); //3016/1508 Delay to load the FPGA
       }
-      else if (x == '1'){
-
-        //pulse width measurement
-//        int timeTracker = 0;
-//        int result = 0;
-//        int i = 0;
-//        while (timeTracker <= 1050){ // 1.05ms
-//          long tempResult = pulseIn(analogPin, HIGH, 1050);
-//          if (tempResult >= 1000){
-//            result = tempResult;
-//            break;
-//          }
-//          timeTracker += tempResult;
-//          buf[i]= tempResult;
-//          i++;
-//        }
-
+      else if (x == pulseCountMeasureSelection){
+        Serial.print("START\nSTART\nSTART\n");
         //PULSE COUNT   //using interrupt
         cli(); //disable interrupt pin
         pulseCount = 0;
         sei(); //enable interrupt pin
+
+        // Check to see if this is actually one second
         delay(1000);
         Serial.println(pulseCount);
+        Serial.print("FINISHED\nFINISHED\nFINISHED\n");
+        delay(10); //3016/1508 Delay to load the FPGA
+      }
 
+      else if(x == pulseWidthMeasureSelection)
+      {
+        Serial.print("START\nSTART\nSTART\n");
+       //pulse width measurement
+       int timeTracker = 0;
+       int result = 0;
+       int i = 0;
+
+       while (timeTracker <= 1050){ // 1.05ms
+         long tempResult = pulseIn(analogPin, HIGH, 1050);
+         if (tempResult >= 1000){
+           result = tempResult;
+           break;
+         }
+         timeTracker += tempResult;
+         buf[i]= tempResult;
+         i++;
+       }
+      Serial.print("FINISHED\nFINISHED\nFINISHED\n");
+      delay(10); //3016/1508 Delay to load the FPGA
+      
       }
     }
 }
