7822d1521ed9320f978dc2a3aad62cb92c0a0287
Daniel Gaull <gaulldj@rose-hulman.edu>
2023-09-14 13:14:47 -0400

    Created file for new config building program

---
+++ src/config_builder.py
@@ -0,0 +1,23 @@
+#
+# This runs as a separate program, taking in the input config path and the output path for the final config
+# It will follow the linked list of config files (with the default_config param)
+# and output the final config at the specified location
+
+# TODO: In output, add a comment at the top describing the list of config files it used
+
+import argparse
+
+program_name="config_builder"
+program_description="""This program combines config files together and outputs a single, final config."""
+program_epilog=""
+parser = argparse.ArgumentParser(prog=program_name,
+                                 description=program_description,
+                                 epilog=program_epilog)
+parser.add_argument('-i' '--input', type=str, help="The input config file to use")
+parser.add_argument('-o', '--output', type=str, help="The path to output the result to")
+# NOTE: Could add arg to override the default config
+
+args = parser.parse_args()
+
+input = args.input
+output = args.output
