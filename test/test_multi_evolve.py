8d28918bc6f7ccaf901a92405ada4ceaf96c97de
Isaac Mierow <NA>
2023-10-04 18:42:20 -0400

    completed my first test and figured out pytest

--- test/test_multi_evolve.py
+++ test/test_multi_evolve.py
@@ -1,6 +1,8 @@
 #! /bin/python
-
 import pytest
+
+from collections.abc import Iterator  
+
 #from typing import override
 def override(func):
     """Temporary structure before implement override in python 3.12 released Today... (10/2/2023) apearently."""
@@ -51,19 +53,19 @@ class TestEvolution(Evolution):
     def evolve_call_count(self)->int:
         return self.evolve_call_count
     
-    def assert_evolve_call_contains(self,callIndex,**args):
-        call_argument_dict:dict = self.all_evolve_call_arguments[callIndex]
+    def assert_evolve_call_contains(self,callIndex,args:[(str,str)]):
+        call_argument_dict:dict = self.previous_evolve_argument_list[callIndex]
         for argument in args:
-            if not argument in call_argument_dict:
-                raise IndexError(f"Argument '{argument}' not found in evolve call. Available arguments are: '{call_argument_dict.keys()}'")
+            if not argument[0] in call_argument_dict:
+                raise IndexError(f"Argument '{argument[0]}' not found in evolve call. Available arguments are: '{call_argument_dict.keys()}'")
 
-            assert args[argument] == call_argument_dict[argument], f"Argument '{argument}' did not match on evolve call index {callIndex}. {args[argument]} != {call_argument_dict[argument]}."
+            assert argument[1] == call_argument_dict[argument[0]], f"Argument '{argument[0]}' did not match on evolve call index {callIndex}. {args[argument]} != {call_argument_dict[argument]}."
 
-    def assert_last_evolve_call_contains(self,**args):
-        self.assert_evolve_call_contains(len(self.all_evolve_call_arguments)-1,args)
+    def assert_last_evolve_call_contains(self,args:[(str,str)]):
+        self.assert_evolve_call_contains(len(self.previous_evolve_argument_list)-1,args)
 
-    def assert_all_calls_contain(self,**args):
-        for call_dict_index in range(len(self.all_evolve_call_arguments)):
+    def assert_all_calls_contain(self,args:[(str,str)]):
+        for call_dict_index in range(len(self.previous_evolve_argument_list)):
             self.assert_evolve_call_contains(call_dict_index,args)
 
     def all_evolve_call_arguments(self)->[dict]:
@@ -82,3 +84,35 @@ def test_monkeypatch_stuff(monkeypatch):
                            print_action_only=True)
     
     assert testEvolution.evolve_was_called() == True
+
+@pytest.fixture
+def test_evolution_object(monkeypatch) -> Iterator[TestEvolution]:
+    test_evolution = TestEvolution()
+    monkeypatch.setattr(multi_evolve,"evolution",test_evolution)
+    yield test_evolution
+    #any taredown code goes here
+
+
+
+@pytest.mark.parametrize("configName",[("config1"),
+                                       ("config2"),
+                                       ("config3")])
+def test_single_configRecieved(configName:str,test_evolution_object:TestEvolution):
+    """Verify that when a single config is passed in multiEvolve works properly."""
+    assert not test_evolution_object.evolve_was_called() 
+    multi_evolve.evolve_list_of_configs(
+        configName,
+        base_config="base-config",
+        output_directory="output-directory",
+        experiment_description="experiment-description",
+        print_action_only=False
+        )
+    
+    test_evolution_object.assert_last_evolve_call_contains([(TestEvolution.PRIMARY_CONFIG_PATH,configName),
+                                                            (TestEvolution.BASE_CONFIG_PATH,"base-config"),
+                                                            (TestEvolution.OUTPUT_DIRECTORY,"output-directory"),
+                                                            (TestEvolution.EXPERIMENT_DESCRIPTION,"experiment-description"),
+                                                            (TestEvolution.PRINT_ACTION_ONLY,False)])
+
+
+
