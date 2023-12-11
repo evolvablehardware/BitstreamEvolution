6df3a49d46711efef2f778c8ff23deca0c2cde65
Isaac Mierow <mierowir@rose-hulman.edu>
2023-12-11 14:41:44 -0500

    added a generate all documentation script.

---
+++ docs/generate-all-documentation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Navigate to the docs directory from wherever this file was called.
+
+#Run all bash scripts associated with compiling documentation
+./generate-website.sh
+
+
+
+
+# Navigate back out to the directory we were previously at.
