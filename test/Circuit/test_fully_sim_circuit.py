1df2a7a2ca232568bf142a3a924ff969b1065969
Daniel Gaull <gaulldj@rose-hulman.edu>
2024-10-12 20:47:50 -0400

    finished unit tests for fully sim circuit

--- test/Circuit/test_fully_sim_circuit.py
+++ test/Circuit/test_fully_sim_circuit.py
@@ -2,8 +2,8 @@ from unittest.mock import Mock
 from Circuit.FullySimCircuit import FullySimCircuit
 
 circuit = None
-config = Mock() #patch('Config.Config')
-rand = Mock() #patch('numpy.Generator')
+config = Mock()
+rand = Mock()
 
 sine_funcs = [(lambda x: (x % 2) * 2)] * 100
 circuit = FullySimCircuit(1, 'n/a', config, sine_funcs, rand)
@@ -20,4 +20,31 @@ def test_simple_eval():
     fit = circuit.calculate_fitness()
     assert fit == 0.998
 
+def test_mutate():
+    circuit.inject_bitstream([0] * 100)
+    # Should mutate every value (since all start at 0)
+    config.get_mutation_probability.return_value = 1
+    rand.uniform.return_value = 0
+    circuit.mutate()
+    bitstream = circuit.get_bitstream()
+    for bit in bitstream:
+        assert bit == 1
 
+def test_randomize_all():
+    # Bitstream starts as all 0s, so mock to change it to all 1s
+    circuit.inject_bitstream([0] * 100)
+    rand.integers.return_value = 1
+    circuit.randomize_bitstream()
+    bitstream = circuit.get_bitstream()
+    for bit in bitstream:
+        assert bit == 1
+
+def test_crossover():
+    parent = FullySimCircuit(1, 'n/a', config, sine_funcs, rand)
+    circuit.inject_bitstream([0] * 100)
+    parent.inject_bitstream([1] * 100)
+    circuit.crossover(parent, 50)
+    bitstream = circuit.get_bitstream()
+    for i in range(50):
+        assert bitstream[i] == 1
+        assert bitstream[i + 50] == 0
