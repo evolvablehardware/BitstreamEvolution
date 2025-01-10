61e16cc412a9283107a0f034dab52beb55faa16a
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-01-10 14:38:22 -0500

    Wrote some Arduino code for handling transferability

--- data/ReadSignal/ReadSignal.ino
+++ data/ReadSignal/ReadSignal.ino
@@ -5,11 +5,12 @@ Convert it to digital value and transmit over serial
 The delays are used to control for noise on the serial line.
 */
 
-#define pulseWidthMeasureSelection '3'
 #define pulseCountMeasureSelection '1'
 #define ADCMeasureSelection '2' 
 #define switchConstant '4'
 #define ADCMeasuretdSelection '5'
+#define DIGITAL_PINS 2
+// Only D2 and D3 can be used for interrupts
 
 // A0 is used for ADC readings from the FPGA output pin (variance / tone discriminator)
 int analogPin = A0;
@@ -25,13 +26,13 @@ int interrupt = 2;
 // D4 is used for synchronization between the data-collecting Nano and the signal-generating Nano
 // The data-collecting Nano uses ReadSignal.ino, while the signal-generating Nano uses oscillate.ino
 int synchPin = 4;
-bool led_thingy = false;
 
 int buf[500]; //500 integers at 10 uS intervals = 5 mS
-volatile long pulseCount = 0;
 unsigned long pulseLength = 0;
-int sampleNumber = 5;
 int x = 0;
+int serialBytesRead = 0;
+int providedPin = 0;
+volatile long pulseCounts[DIGITAL_PINS];
 
 /* ---------------------------------- Setup --------------------------------- */
 void setup(){
@@ -58,22 +59,89 @@ void setup(){
     // digitalWrite(2, HIGH); 
 
     // can be changed from RISING to FALLING or CHANGE
-    attachInterrupt(digitalPinToInterrupt(interrupt),pulseCounter, RISING); 
+    attachInterrupt(digitalPinToInterrupt(D2),pulseCounter2, RISING); 
+    attachInterrupt(digitalPinToInterrupt(D3),pulseCounter3, RISING); 
 }
 
+// No elegant/efficient way to curry in C, 
+// this is the fastest solution
+void pulseCounter2() { pulseCounts[0]++; }
+void pulseCounter3() { pulseCounts[1]++; }
 
-// This method is attached to the interrupt to count pulses
-void pulseCounter(){
-    pulseCount++;
+void resetPulseCounts() {
+  int i;
+  for (i = 0; i < DIGITAL_PINS; i++) {
+    pulseCounts[i] = 0;
+  }
+}
+
+int providedPinToAnalog(int providedPin) {
+  switch (providedPin) {
+    case '0':
+      return A0;
+    case '1':
+      return A1;
+    case '2':
+      return A2;
+    case '3':
+      return A3;
+    case '4':
+      return A4;
+    case '5':
+      return A5;
+    case '6':
+      return A6;
+    case '7':
+      return A7;
+  }
+  return A0;
+}
+
+int providedPinToDigitalIndex(int providedPin) {
+  switch (providedPin) {
+    case '2':
+      return 0;
+    case '3':
+      return 1;
+    case '4':
+      return 2;
+    case '5':
+      return 3;
+    case '6':
+      return 4;
+    case '7':
+      return 5;
+    case '8':
+      return 6;
+    case '9':
+      return 7;
+    case 'a':
+      return 8;
+    case 'b':
+      return 9;
+    case 'c':
+      return 10;
+  }
+  return 0;
 }
 
 /* ---------------------------------- Loop ---------------------------------- */
 void loop(){
-    if ((x = Serial.available()) > 0){
+    if ((serialBytesRead = Serial.available()) > 0){
+      // NOTE: Serial.read() reads in one byte at a time
       int x = Serial.read();
+      // Backwards-compatible; handles if pin not provided
+      // A pin of -1 will fallback to A0/D2 (what we used to use)
+      if (serialBytesRead > 1) {
+        providedPin = Serial.read();
+      } else {
+        providedPin = -1;
+      }
       
       if (x == ADCMeasureSelection){
         Serial.print("START\nSTART\nSTART\n");
+
+        int analogPin = providedPinToAnalog(providedPin);
 
         for(int i=0; i<=499; i++){
             Serial.print(i+1);
@@ -126,45 +194,21 @@ void loop(){
       }
       else if (x == pulseCountMeasureSelection){
         //Serial.print("START\nSTART\nSTART\n"); // currently breaks measure_pulses()
+        int idx = providedPinToDigitalIndex(providedPin);
         //PULSE COUNT   //using interrupt
         cli(); //disable interrupt pin
-        pulseCount = 0;
+        resetPulseCounts();
         sei(); //enable interrupt pin
 
         // Check to see if this is actually one second
         delay(1000);
-        Serial.println(pulseCount);
+        Serial.println(pulseCounts[idx]);
         // Serial.print("FINISHED\nFINISHED\nFINISHED\n");  // currently breaks measure_pulses()
         delay(10); //3016/1508 Delay to load the FPGA
       }
-
-      else if(x == pulseWidthMeasureSelection)
-      {
-        Serial.print("START\nSTART\nSTART\n");
-       //pulse width measurement
-       int timeTracker = 0;
-       int result = 0;
-       int i = 0;
-
-       while (timeTracker <= 1050){ // 1.05ms
-         long tempResult = pulseIn(analogPin, HIGH, 1050);
-         if (tempResult >= 1000){
-           result = tempResult;
-           break;
-         }
-         timeTracker += tempResult;
-         buf[i]= tempResult;
-         i++;
-       }
-
-      Serial.print(result);
-      Serial.print("FINISHED\nFINISHED\nFINISHED\n");
-      delay(10); //3016/1508 Delay to load the FPGA
-      
-      }
-
       else if(x == switchConstant)
       {
+        // TODO: Old transferability system will not work on this branch
         detachInterrupt(digitalPinToInterrupt(interrupt));
         if(analogPin == A0) {
           analogPin = A6;
---
+++ evolvatron.md
@@ -0,0 +1,7 @@
+## For documenting current state of the "evolvatron" implementation
+
+NOTE: Currently only implemented for simple variance maximization and pulse count experiments
+
+Does not work for tone discrimination at this time
+
+Only D2 and D3 can be used for interrupts
