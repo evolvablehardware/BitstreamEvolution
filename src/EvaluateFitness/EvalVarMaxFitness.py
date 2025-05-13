0bd07d3ee3c560768e88c58be622f0cf9f17a4c9
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-05-13 14:44:09 -0400

    Var-max fitness eval impl

---
+++ src/EvaluateFitness/EvalVarMaxFitness.py
@@ -0,0 +1,30 @@
+# For use with EvaluateFitness: 
+# eval_var_max = EvalVarMaxFitness()
+# EvaluateFitness(eval_var_max.calculate_success, eval_var_max.calculate_err)
+class EvalVarMaxFitness:
+    def __init__(self):
+        pass
+
+    def calculate_success(self, data: list[int]) -> float:
+        waveform = data
+        variance_sum = 0
+        total_samples = 500
+        for i in range(len(waveform)-1):
+            # NOTE Signal Variance is calculated by summing the absolute difference of
+            # sequential voltage samples from the microcontroller.
+            # Capture the next point in the data file to a variable
+            initial1 = waveform[i]
+            # Capture the next point + 1 in the data file to a variable
+            initial2 = waveform[i+1]
+            # Take the absolute difference of the two points and store to a variable
+            variance = abs(initial2 - initial1)
+
+            if initial1 != None and initial1 < 1000:
+                variance_sum += variance
+
+        fitness = variance_sum / total_samples
+
+        return fitness
+    
+    def calculate_err(self, err: Exception) -> float:
+        return 0
